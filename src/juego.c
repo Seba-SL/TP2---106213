4293d1c8e14522424c6e82457ff2c3b773e5b5d8
Lazo Sebastian <slazo@fi.uba.ar>
2023-11-30 06:46:51 -0300

    jueves

--- src/juego.c
+++ src/juego.c
@@ -30,6 +30,10 @@ struct juego
 	size_t cantidad_movimientos;
 };
 
+
+enum TIPO asignar_tipo( char inicialTipo);
+RESULTADO_ATAQUE asignar_efectividad(enum TIPO atk_pkm1,enum TIPO tipo_pkm2);
+
 juego_t *juego_crear()
 {	
 
@@ -161,6 +165,138 @@ JUEGO_ESTADO juego_seleccionar_pokemon(juego_t *juego, JUGADOR jugador,const cha
 	return TODO_OK;
 }
 
+
+resultado_jugada_t asignar_resultado(enum TIPO tipo_pkm1,enum TIPO tipo_pkm2,enum TIPO atk_pkm1,enum TIPO atk_pkm2)
+{
+	resultado_jugada_t resultado;
+
+	resultado.jugador1 = asignar_efectividad(atk_pkm1,tipo_pkm2);
+	resultado.jugador2 = asignar_efectividad(atk_pkm2,tipo_pkm1);
+
+	
+	return resultado;
+}
+
+resultado_jugada_t juego_jugar_turno(juego_t *juego, jugada_t jugada_jugador1,jugada_t jugada_jugador2)
+{
+	resultado_jugada_t resultado;
+
+	resultado.jugador1 = ATAQUE_ERROR;
+	resultado.jugador2 = ATAQUE_ERROR;
+
+	if(juego == NULL)return resultado;
+
+	pokemon_t *pkm_1 = lista_buscar_elemento(juego->lista_pokemones,comparar_nombres,jugada_jugador1.pokemon);
+	pokemon_t *pkm_2 = lista_buscar_elemento(juego->lista_pokemones,comparar_nombres,jugada_jugador2.pokemon);
+
+	enum TIPO tipo_pkm1 = pokemon_tipo(pkm_1);
+	enum TIPO atk_pkm1 = asignar_tipo(jugada_jugador1.ataque[0]);
+	
+	enum TIPO tipo_pkm2 = pokemon_tipo(pkm_2);
+	enum TIPO atk_pkm2 = asignar_tipo(jugada_jugador2.ataque[0]);
+
+	if(atk_pkm1 == NORMAL && atk_pkm2 == NORMAL)
+	{ 
+		resultado.jugador1 = ATAQUE_REGULAR;
+		resultado.jugador1 = ATAQUE_REGULAR;
+			return resultado;
+	}
+
+	return asignar_resultado(tipo_pkm1,tipo_pkm2,atk_pkm1,atk_pkm2);
+
+	
+}
+
+int juego_obtener_puntaje(juego_t *juego, JUGADOR jugador)
+{
+	if(juego == NULL)
+		return 0;
+
+	return juego->jugadores[jugador].puntaje;
+}
+
+bool juego_finalizado(juego_t *juego)
+{
+	if(juego == NULL)return true;
+
+	if (juego->cantidad_movimientos == 0)return true;
+
+	return false;
+}
+
+void juego_destruir(juego_t *juego)
+{
+	
+	lista_destruir_todo(juego->lista_pokemones, NULL);
+	free(juego);
+
+}
+
+
+enum TIPO asignar_tipo( char inicialTipo)
+{
+    //NORMAL, FUEGO, AGUA, PLANTA, ELECTRICO, ROCA
+
+    switch(inicialTipo)
+    {
+        case 'N':
+            return NORMAL;
+            break;
+		
+		case 'n':
+            return NORMAL;
+            break;
+
+        case 'F':
+            return FUEGO;
+            break;
+		
+		case 'f':
+            return FUEGO;
+            break;
+        
+        
+        case 'A':
+            return AGUA;
+            break;
+		
+		case 'a':
+            return AGUA;
+            break;
+
+
+        case 'P':
+            return PLANTA;
+            break;
+  		
+		case 'p':
+            return PLANTA;
+            break;	
+        
+		case 'E':
+            return ELECTRICO;
+            break;
+		case 'e':
+            return ELECTRICO;
+            break;
+
+        case 'R':
+            return ROCA;
+            break;
+        
+		case 'r':
+            return ROCA;
+            break;
+        
+        
+        default : 
+            return NORMAL;
+
+    }
+
+}
+
+
 RESULTADO_ATAQUE asignar_efectividad(enum TIPO atk_pkm1,enum TIPO tipo_pkm2)
 {
 
@@ -237,107 +373,4 @@ if(atk_pkm1 == FUEGO)
 
 	return ATAQUE_REGULAR;
 	
-}
-
-resultado_jugada_t asignar_resultado(enum TIPO tipo_pkm1,enum TIPO tipo_pkm2,enum TIPO atk_pkm1,enum TIPO atk_pkm2)
-{
-	resultado_jugada_t resultado;
-
-	resultado.jugador1 = asignar_efectividad(atk_pkm1,tipo_pkm2);
-	resultado.jugador2 = asignar_efectividad(atk_pkm2,tipo_pkm1);
-
-	
-	return resultado;
-}
-enum TIPO asignar_tipo( char inicialTipo)
-{
-    //NORMAL, FUEGO, AGUA, PLANTA, ELECTRICO, ROCA
-
-    switch(inicialTipo)
-    {
-        case 'N':
-            return NORMAL;
-            break;
-
-        case 'F':
-            return FUEGO;
-            break;
-        
-        case 'A':
-            return AGUA;
-            break;
-
-         case 'P':
-            return PLANTA;
-            break;
-
-        case 'E':
-            return ELECTRICO;
-            break;
-
-        case 'R':
-            return ROCA;
-            break;
-        
-        
-        
-        default : 
-            return NORMAL;
-
-    }
-
-}
-resultado_jugada_t juego_jugar_turno(juego_t *juego, jugada_t jugada_jugador1,jugada_t jugada_jugador2)
-{
-	resultado_jugada_t resultado;
-
-	resultado.jugador1 = ATAQUE_ERROR;
-	resultado.jugador2 = ATAQUE_ERROR;
-
-	if(juego == NULL)return resultado;
-
-	pokemon_t *pkm_1 = lista_buscar_elemento(juego->lista_pokemones,comparar_nombres,jugada_jugador1.pokemon);
-	pokemon_t *pkm_2 = lista_buscar_elemento(juego->lista_pokemones,comparar_nombres,jugada_jugador2.pokemon);
-
-	enum TIPO tipo_pkm1 = pokemon_tipo(pkm_1);
-	enum TIPO atk_pkm1 = asignar_tipo(jugada_jugador1.ataque[0]);
-	
-	enum TIPO tipo_pkm2 = pokemon_tipo(pkm_2);
-	enum TIPO atk_pkm2 = asignar_tipo(jugada_jugador2.ataque[0]);
-
-	if(atk_pkm1 == NORMAL && atk_pkm2 == NORMAL)
-	{ 
-		resultado.jugador1 = ATAQUE_REGULAR;
-		resultado.jugador1 = ATAQUE_REGULAR;
-			return resultado;
-	}
-
-	return asignar_resultado(tipo_pkm1,tipo_pkm2,atk_pkm1,atk_pkm2);
-
-	
-}
-
-int juego_obtener_puntaje(juego_t *juego, JUGADOR jugador)
-{
-	if(juego == NULL)
-		return 0;
-
-	return juego->jugadores[jugador].puntaje;
-}
-
-bool juego_finalizado(juego_t *juego)
-{
-	if(juego == NULL)return true;
-
-	if (juego->cantidad_movimientos == 0)return true;
-
-	return false;
-}
-
-void juego_destruir(juego_t *juego)
-{
-	
-	lista_destruir_todo(juego->lista_pokemones, NULL);
-	free(juego);
-
 }
